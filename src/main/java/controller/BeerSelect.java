5bed81a731b732cd99e1878ada1a43f22a7d0822
GetanehYilma <gyletike@mum.edu>
2019-09-19 23:04:11 -0500

    first commit

---
+++ src/main/java/controller/BeerSelect.java
@@ -0,0 +1,34 @@
+package controller;
+
+import model.BeerExpert;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class BeerSelect extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String c = request.getParameter("color");
+        BeerExpert be = new BeerExpert();
+        List result = be.getBrands(c);
+
+        //response.setContentType("text/html");
+        //PrintWriter out = response.getWriter();
+        //out.println("Beer Selection Advice<br>");
+        //Iterator it = result.iterator();
+        //while(it.hasNext()) {
+        //out.print("<br>try: " + it.next());
+
+
+
+        request.setAttribute("styles", result);
+        RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+        //Use the request dispatcher to ask the Container to crank up the JSP, sending it the request and response.
+        view.forward(request, response);
+
+    }
+}
